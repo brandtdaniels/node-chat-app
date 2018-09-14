b8dbbe963d0459197d304f4fa0e4419f724f74b9
Brandt Daniels <brandtdaniels@gmail.com>
2018-09-14 15:32:02 -0700

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -44,10 +44,14 @@ jQuery('#message-form').on('submit', function (event) {
 
   event.preventDefault()
 
+  var messageTextbox = jQuery('[name=message]')
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function () {
+
+    messageTextbox.val('')
 
   })
 
@@ -63,7 +67,11 @@ locationButton.on('click', function () {
 
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...')
+
   navigator.geolocation.getCurrentPosition(function (position) {
+
+    locationButton.removeAttr('disabled').text('Send location')
 
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
@@ -71,6 +79,8 @@ locationButton.on('click', function () {
     })
 
   }, function () {
+
+    locationButton.removeAttr('disabled').text('Send location')
 
     alert('Unable to fetch location.')
 
