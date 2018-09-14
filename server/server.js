bef44d04bb3578035c0f422c5d418f4f9a74fac8
Brandt Daniels <brandtdaniels@gmail.com>
2018-09-14 11:00:09 -0700

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -17,6 +17,18 @@ io.on('connection', (socket) => {
 
   console.log('New user connected')
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to TribalChat!',
+    createdAt: new Date().getTime()
+  })
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  })
+
   socket.on('createMessage', (message) => {
 
     console.log('createMessage', message)
@@ -26,6 +38,12 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     })
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // })
 
   })
 
@@ -40,5 +58,5 @@ io.on('connection', (socket) => {
 server.listen(port, () => {
 
   console.log(`Server is up on port ${port}`)
-  
+
 })
